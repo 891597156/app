a4f7e78f1e31cb119957f21d8ad6cdd48d9ef22b
donghong <891597156@qq.com>
2022-08-27 21:27:14 +0800

    lins

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- 三级联动组件：三级联动已经注册为全局组件，因此不需要映入 -->
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <!-- <Like></Like> -->
+  </div>
+</template>
+
+<script>
+import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+export default {
+  name: "Home",
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- <h1>params参数--{{ $route.params.keyword }}</h1>
+    <h1>query参数--{{ $route.query }}</h1> -->
+
+    <!-- 通过简写 -->
+    <h1>params参数--{{ keyword }}</h1>
+    <h1>query参数--{{ k }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  // 路由组件是可以传递props
+  props:['keyword','k'],
+};
+</script>
+
+<style>
+</style>
